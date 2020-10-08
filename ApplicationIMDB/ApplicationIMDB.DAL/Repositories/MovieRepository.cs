0d9b05f6fa381d08fc7edb500522ae0e0c5ddddc
YRAELIAS-PC\dav23 <davidflemal@gmail.com>
2020-10-08 15:15:17 +0200

    APIMDB-4 Add Movie Repository + Service

---
+++ ApplicationIMDB/ApplicationIMDB.DAL/Repositories/MovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationIMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationIMDB.DAL.Repositories
+{
+    class MovieRepository : BaseRepository<Movie,int>
+    {
+        public IEnumerable<Movie> Get(string query)
+        {
+            return base.ExecuteReader(query);
+        }
+    }
+}
---
+++ ApplicationIMDB/ApplicationIMDB.DAL/Services/MovieService.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationIMDB.DAL.Repositories;
+using ApplicationIMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationIMDB.DAL.Services
+{
+    class MovieService : BaseService<Movie, int>
+    {
+        MovieRepository repo;
+        public MovieService()
+        {
+            repo = new MovieRepository();
+        }
+        public override bool Add(Movie id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(Movie id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Movie> Get()
+        {
+            return repo.ExecuteReader("SELECT * FROM T_Movies");
+        }
+
+        public override Movie GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(Movie obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
