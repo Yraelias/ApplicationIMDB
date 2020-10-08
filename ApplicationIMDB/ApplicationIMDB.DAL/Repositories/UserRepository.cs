ad766d7cf377150be06d08747f3504a708da8d22
YRAELIAS-PC\dav23 <davidflemal@gmail.com>
2020-10-08 12:20:53 +0200

    APIMDB-9 Add User in DAL

---
+++ ApplicationIMDB/ApplicationIMDB.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationIMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationIMDB.DAL.Repositories
+{
+    class UserRepository : BaseRepository<User,int>
+    {
+        public IEnumerable<User> Get(string query)
+        {
+            return base.ExecuteReader(query);
+        }
+    }
+}
---
+++ ApplicationIMDB/ApplicationIMDB.DAL/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationIMDB.DAL.Repositories;
+using ApplicationIMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationIMDB.DAL.Services
+{
+    class UserService : BaseService<User, int>
+    {
+        UserRepository repo;
+        public UserService()
+        {
+            repo = new UserRepository();
+        }
+        public override bool Add(User id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(User id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<User> Get()
+        {
+            return repo.ExecuteReader("SELECT * FROM T_Users");
+        }
+
+        public override User GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(User obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
